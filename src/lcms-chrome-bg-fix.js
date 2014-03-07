d96e4b9811111d998fde13c5677dd1ce20ac950e
joe.landsman@dnlomnimedia.com <joe.landsman@dnlomnimedia.com>
2014-03-07 17:27:48 -0500

    Reworked methodology so that fix is applied any time LOM session is refreshed

--- src/lcms-chrome-bg-fix.js
+++ src/lcms-chrome-bg-fix.js
@@ -2,7 +2,7 @@
  * Name: Luminate CMS Chrome Background Fix
  * Author: DNL Omnimedia Inc. <info@dnlomnimedia.com>
  * Date: 1/15/2014
- * Version: .9
+ * Version: .91
  * Copyright: 2014 DNL OmniMedia Inc.
  * License: The MIT License (MIT)
  *
@@ -13,8 +13,8 @@
     // include any stylesheets which contain references to background images.
     var stylesheetSelector = 'link[rel="stylesheet"]';
 
-    // Name of cookie used to determine if this script ran recently
-    var cookieName = 'lcms_chrome_bg_fix_ran';
+    // Name of cookie used by this script
+    var cookieName = 'lcms_chrome_bg_fix_session_id';
 
     // Determine if the current browser is Google Chrome
     function isChrome() {
@@ -27,7 +27,7 @@
     }
 
     // Clone each of the provided stylesheets, change their 'rel' to 'prefetch', inject them
-    // into the DOME then change their 'rel' back to 'stylesheet'. This is the special sauce
+    // into the DOM then change their 'rel' back to 'stylesheet'. This is the special sauce
     // which forces Chrome to reprocess the stylesheets. Also, unlike simply performing a 'rel' 
     // switch on the existing stylesheets this approach does not cause the site to lose all of
     // its styles and become temporarily unstyled. Che bella!
@@ -41,19 +41,24 @@
         }
     }
 
-    // Return TRUE if this is the first time the script has run (based on an empty cookie). If
-    // first time then set cookie value and return TRUE.
-    function isFirstRun() {
-        var cookie = getCookie(cookieName);
-        if(cookie == "") {
-            setCookie(cookieName, "true", 1);
-            return true;
-        }
-        return false;
+    // Determine whether the Luminate session has changed since the previous page load
+    function sessionChanged() {
+    	var cookie = getCookie(cookieName);
+    	var sessionId = (CONVIO || {}).pageSessionID;
+
+    	if(cookie == sessionId) {
+    		return false;
+    	} else {
+    		setCookie(cookieName, sessionId, 1000);
+    		return true;
+    	}
     }
 
-    // If visitor is using Chrome then perform prefetch injections
-    if(isChrome() && isFirstRun()) {
+    // If visitor is using Chrome and the session id has changed since our last run
+    // then perform prefetch injections. In this way, the prefetch will run whenver
+    // the session has been revalidated. Meaning any time Luminate fires the
+    // asynchronous redirect that breaks BG images we will run our fix.
+    if(isChrome() && sessionChanged()) {
         injectPrefetches( getStylesheets() );
     }
 
